beefda7037fa18c46b68052dc155379be6cc830d
ppiod <piodfallen@gmail.com>
2025-10-08 13:55:07 -0300

    added explanatory comments to EventRegistry

--- src/discord/base/registry/EventRegistry.ts
+++ src/discord/base/registry/EventRegistry.ts
@@ -1,13 +1,16 @@
 import type { ClientEvents } from "discord.js";
 
+/** Defines the structure of an event listener. */
 export interface Event<K extends keyof ClientEvents> {
   name: K;
   once?: boolean;
   run: (...args: ClientEvents[K]) => any | Promise<any>;
 }
 
+/** The central registry for all events. */
 export const eventRegistry = new Map<keyof ClientEvents, Event<any>>();
 
+/** Registers an event to the registry. */
 export function registerEvent<K extends keyof ClientEvents>(event: Event<K>) {
   eventRegistry.set(event.name, event);
 }
